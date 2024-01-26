aaaef5fff356f974a50da3f54bbd32a2919fe638
mxs980 <mxs980@aol.com>
2024-01-26 13:47:33 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,4 @@
+// INPUT_REQUIRED {Add configuration variables and settings here}
+# Add configuration variables and settings here
+variable1 = "value1"
+variable2 = 123
---
+++ main.py
@@ -0,0 +1,4 @@
+from chat_pdf.ui.app import ChatPdfApp
+
+if __name__ == '__main__':
+    ChatPdfApp().run()
